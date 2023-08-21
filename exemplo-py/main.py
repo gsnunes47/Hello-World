2cba4383c18e573bf0f79500c7d6b764311f41dc
Gustavo Nunes <gustavompn06@gmail.com>
2023-08-21 12:46:06 -0300

    Transformei o script em uma função.

--- exemplo-py/main.py
+++ exemplo-py/main.py
@@ -1,5 +1,9 @@
+def fat(n):
+    fatorial = 1
+    for fat in range(1, numero + 1):
+        fatorial *= fat
+    return fatorial
+
+
 numero = int(input('Digite um número para ver seu fatorial: '))
-fatorial = 1
-for fat in range(1, numero + 1):
-    fatorial *= fat
-print(f'O fatorial de {numero} é {fatorial}.')
+print(f"O fatorial de {numero} é {fat(numero)}.")
